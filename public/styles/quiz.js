7b6a8caaf85c9c5824ec0e716d1fa489babb4df5
cfajitas <cfajitas@gmail.com>
2016-11-30 00:31:05 +0000

    added new quiz elements

--- public/styles/quiz.js
+++ public/styles/quiz.js
@@ -1,5 +1,5 @@
 (function() {
-  var questions = [{
+  var data1 = [{
     question: "Pick the undesirable user experience goal:",
     choices: ["Enjoyable", "Exciting", "Boring", "Helpful", "Rewarding"],
     correctAnswer: 3
@@ -21,12 +21,87 @@
     correctAnswer: 1
   }];
   
+	var data2 = [{
+  	question: "These are the advantages of __________ : Can be used early on, inexpensive and easy to create, make design idea visual, no special knowledge is required and all team members can creat them.",
+ 	 choices: ["Personas", "Direct Manipulation", "Rapid Prototyping", "Lo-fi prototype"],
+  	correctAnswer: 4
+	}, {
+  	question: "The following statements are disadvantages of __________: They can be dificult to create if the target audience is international, Too much will make the work difficult, There is a risk of incoroporating unsupported designer assumption.",
+  	choices: ["Card Sort", "Personas ", "Menu-based Interface", "Semantics"],
+  	correctAnswer: 2
+	}, {
+  	question: "Observation and elicitation are :",
+  	choices: ["Tools for Physical Design", "Advantages of natural language", "Types of Observation", "Methods of Collection"],
+  	correctAnswer: 1
+	}, {
+  	question: "Which of he following are not one of Nielson's Heuristics for User Interface Design?",
+  	choices: ["Error Prevention", "Consistency and Standards", "Help and Documentation", "Encoding", "Recognition Rather Than Recall"],
+  	correctAnswer: 4
+	}, {
+  	question: "Participants, design and testers are: ",
+  	choices: ["Three phases in direct manipulation", "Three basic components for usability testing", "Three main criteria for lo-fi prototype", "Three main criteria for hi-fi prototype"],
+  	correctAnswer: 2
+	}];
+	
+	var data3 = [{
+  	question: "These are the advantages of __________ : Ease of Learning, Low memory requirement, flexible interaction, low screen requirement, appropriate for beginner",
+  	choices: ["Commmand line", "Natural Language", "Menu-based Interface", "Usability Testing"],
+  	correctAnswer: 2
+  }, {
+  	question: "Time, finance, personnel and laboratory are all _______",
+  	choices: ["Advantages of usability testing", "Interaction styles", "Constraints on usability testing", "Obstacles of natural language"],
+  	correctAnswer: 3 
+  }, {
+  	question: "The following are _________________: Easy and inexpesive to make, flexible enough to be constantly changed and rearranged, complete enough to yield useful feedback about specific design questions.",
+  	choices: ["Interface design standard tools", "Main criteria for lo-fi prototype", "Main criteria for hi-fi prototype", "Main criteria for conceptual design"],
+  	correctAnswer: 2    	
+  }, {
+  	question: "The following are disadvantages of ___________: Rapid and inflexible navigation, inefficient for large menu navigation, infficient use of screen real estate, slow for experts",
+  	choices: ["Natural Language", "Question and Answer", "Menu-based interface", "Direct Manipulation"],
+  	correctAnswer: 3 
+  }, {
+  	question: "Gulfs of ______ relate to the effectiveness principle",
+  	choices: ["Evaluation", "Execution", "Natural Language", "Examination"],
+  	correctAnswer: 2
+  }];
+  
   var questionCounter = 0; //Tracks question number
   var selections = []; //Array containing user choices
   var quiz = $('#quiz'); //Quiz div object
   
-  // Display initial question
-  displayNext();
+  var questions;
+  $('#next').hide();
+  //displayNext(); hides initial quiz
+  
+  $('#set1').on('click', function (e){
+    e.preventDefault()
+    questions = data1;
+    questionCounter = 0;
+    selections = [];
+    $('#start').hide();
+    $('#next').show();
+    displayNext();
+  });
+  
+  $('#set2').on('click', function (e){
+    e.preventDefault()
+    questions = data2;
+    questionCounter = 0;
+    selections = [];
+    $('#start').hide();
+    $('#next').show();
+    displayNext();
+  });
+  
+  $('#set3').on('click', function (e){
+    e.preventDefault()
+    questions = data3;
+    questionCounter = 0;
+    selections = [];
+    $('#start').hide();
+    $('#next').show();
+    displayNext();
+  });
   
   // Click handler for the 'next' button
   $('#next').on('click', function (e) {
